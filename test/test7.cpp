881b3ec7e009a6615a719ced6fddbfe09b517b64
kiddos <josephyu831007@yahoo.com.tw>
2016-04-29 05:38:59 +0800

    display plot

--- test/test7.cpp
+++ test/test7.cpp
@@ -6,9 +6,8 @@
 #include <time.h>
 #include <string>
 
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/shape.hpp>
+#include <mgl2/qt.h>
+#include <mgl2/mgl.h>
 
 #include "../src/nnet.h"
 
@@ -24,7 +23,7 @@ using nn::mat;
 const int K = 10;
 const int datasize = 188;
 const double scale = 5000;
-const double tou = 1000;
+//const double tou = 1000;
 
 double rectifier(double z) {
   return z >= 0 ? z : 0;
@@ -53,12 +52,12 @@ double identitygrad(double) {
 }
 
 mat cost(mat y, mat h) {
-  const mat diff = h - y;
-  const mat squared = (diff % diff) / tou;
-  mat J = tou * nn::funcop(squared, exp);
+  //const mat diff = h - y;
+  //const mat squared = (diff % diff) / tou;
+  //mat J = tou * nn::funcop(squared, exp);
 
-  //const mat diff = y - h;
-  //const mat J = (diff % diff) / 2.0;
+  const mat diff = y - h;
+  const mat J = (diff % diff) / 2.0;
   //mat J = -(y % h.transform(log) + (1-y) % nn::funcop(1-h, log));
   //mat J = -(y % h.transform(log) + (1-y) % nn::funcop(1-h, log)) / y.n_rows;
   //mat J = -(y % h);
@@ -66,11 +65,11 @@ mat cost(mat y, mat h) {
 }
 
 mat costd(mat y, mat a, mat,mat) {
-  const mat diff = a - y;
-  const mat squared = (diff % diff) / tou;
-  const mat J = tou * nn::funcop(squared, exp);
-  const mat grad = (2.0/tou) * diff % J;
-  //const mat grad = (a - y);
+  //const mat diff = a - y;
+  //const mat squared = (diff % diff) / tou;
+  //const mat J = tou * nn::funcop(squared, exp);
+  //const mat grad = (2.0/tou) * diff % J;
+  const mat grad = (a - y);
   //mat grad = (a - y);
   //mat grad = (a - y) / y.n_rows;
   //mat grad = -(y % nn::funcop(z, sigmoidgrad));
@@ -121,18 +120,32 @@ double accuracy(mat answer, mat prediction) {
   return correct / static_cast<double>(answer.n_rows);
 }
 
+mglData prediction;
+mglData answer;
+int sample(mglGraph* graph) {
+  graph->AddLegend("prediction", "r");
+  graph->AddLegend("answer", "b");
+  graph->SetRanges(1991, 1996, 0, 130000);
+  graph->Axis("y");
+  graph->Axis("x");
+  graph->Label('y', "number of pig slaugher");
+  graph->Label('x', "time");
+  graph->Plot(prediction, "-r5");
+  graph->Plot(answer, "-b5");
+  return 0;
+}
+
 int main() {
   double lrate = 1e-5;
-  const double lratedecay = 1e-9;
-  const double lambda = 999e-3;
+  const double lambda = 1e-7;
 
   srand(time(NULL));
 
   InputLayer input(K);
   vector<Layer> hidden = {
-    Layer(K, K, lrate, lambda, atan, [](double x) {return 1.0 / (1.0+x*x);}),
+    Layer(K, K * 10, lrate, lambda, atan, [](double x) {return 1.0 / (1.0+x*x);}),
   };
-  OutputLayer output(K, 1, lrate, lambda, identity, identitygrad, cost, costd);
+  OutputLayer output(K * 10, 1, lrate, lambda, identity, identitygrad, cost, costd);
   NeuralNet nnet(input, output, hidden);
 
   mat x, y;
@@ -140,23 +153,47 @@ int main() {
   //cout << x << endl;
   //cout << y << endl;
 
+  const int batchsize = x.n_rows / 3;
   //nnet.feeddata(x, y, true);
-  for (int i = 0 ; i < 260000 ; ++i) {
-    //nnet.feeddata(x.row(i % (datasize-K-1)), y.row(i % (datasize-K-1)), false);
-    nnet.feeddata(x, y, false);
-    if (i > 80000) {
-      nnet.setlrate(lrate);
-      if (lrate > 1e-7)
-        lrate -= lratedecay;
+  for (uint32_t i = 0 ; i < x.n_rows * 2000 ; ++i) {
+    const int start = i % (x.n_rows-batchsize);
+    const int end = start + batchsize;
+    nnet.feeddata(x.rows(start, end), y.rows(start, end), false);
+    //nnet.feeddata(x.row(i % x.n_rows), y.row(i % x.n_rows), false);
+
+    const double newcost = nnet.computecost();
+    cout << "\riteration: " << i+1 << " cost: " << newcost;
+    if (i % (x.n_rows * 10) == 0) {
+      cout << endl << "iteration: " << i+1 << " cost: " << nnet.computecost();
     }
-    cout << "\riteration: " << i+1 << " cost: " << nnet.computecost();
+    if (newcost < 100 && i % (x.n_rows * 10) == 0) {
+      lrate *= 0.98;
+      nnet.setlrate(lrate);
+    }
+    //nnet.feeddata(x, y, false);
+    //if (i > 70000) {
+      //nnet.setlrate(lrate);
+      //if (lrate > 1e-7) {
+        //lrate *= lratedecay;
+      //}
+    //}
   }
   cout << endl;
   mat result = nnet.predict(x);
+  //for (uint32_t i = 0 ; i < y.n_rows ; ++i) {
+    //cout << "predict: " << result(i, 1) * scale <<
+        //" | answer: " << y(i, 0) * scale << endl;
+  //}
+
+  prediction = mglData(result.n_rows);
+  answer = mglData(y.n_rows);
   for (uint32_t i = 0 ; i < y.n_rows ; ++i) {
-    cout << "predict: " << result(i, 1) * scale <<
-        " | answer: " << y(i, 0) * scale << endl;
+    prediction.a[i] = result(i, 1) * scale;
+    answer.a[i] = y(i, 0) * scale;
   }
+  mglQT display(sample, "monthly total number of pigs slaughter");
+
+  return display.Run();
 
   return 0;
 }
