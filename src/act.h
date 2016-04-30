09156059f79823b253bd92702d5dd5c5d906d778
kiddos <josephyu831007@yahoo.com.tw>
2016-04-30 12:47:41 +0800

    add relu

--- src/act.h
+++ src/act.h
@@ -15,16 +15,16 @@ ActFunc initactfunc(double (*act)(double), double (*actd)(double));
 extern ActFunc identity;
 extern ActFunc sigmoid;
 extern ActFunc arctan;
+extern ActFunc comploglog;
 extern ActFunc tanhyperbolic;
 extern ActFunc bipolarsigmoid;
 extern ActFunc lecuntanh;
 extern ActFunc hardtanh;
 extern ActFunc absolute;
+extern ActFunc relu;
 extern ActFunc relucos;
 extern ActFunc relusin;
-extern ActFunc smoothrectifier;
-extern ActFunc logit;
-extern ActFunc cosine;
+extern ActFunc smoothrelu;
 
 } /* end of nn namespace */
 
