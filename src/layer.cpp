49ea873085dcf3f7e4e89cf2275953ed36a50d8d
kiddos <josephyu831007@yahoo.com.tw>
2016-05-03 09:59:38 +0800

    bug fix softmax delta wrong sign

--- src/layer.cpp
+++ src/layer.cpp
@@ -328,7 +328,7 @@ mat SoftmaxOutput::costfuncdelta(mat y, mat a, mat) {
   const mat expo = arma::exp(a);
   const mat sumexpo = arma::repmat(arma::sum(expo, 1), 1, y.n_cols);
   const mat P = expo % (1.0 / sumexpo);
-  const mat delta = y - P;
+  const mat delta = P - y;
   return delta;
 }
 
