781797349d73c5b7d5d15140165746f371965dd0
kiddos <josephyu831007@yahoo.com.tw>
2016-05-05 17:38:38 +0800

    movement_libras experiment

--- test/test12.cpp
+++ test/test12.cpp
@@ -25,74 +25,6 @@ const int datasize = 360;
 const int n = 90;
 const int o = 15;
 
-double rectifier(double z) {
-  return z >= 0 ? z : 0;
-}
-
-double rectifiergrad(double z) {
-  return z >= 0 ? 1 : 0;
-}
-
-double sigmoid(double z) {
-  return 1.0 / (1.0 + exp(-z));
-}
-
-double sigmoidgrad(double z) {
-  const double e = exp(-z);
-  const double b = 1 + e;
-  return e / (b * b);
-}
-
-double identity(double z) {
-  return z;
-}
-
-double identitygrad(double) {
-  return 1;
-}
-
-
-mat colsum(const mat m) {
-  mat result(m.n_rows, m.n_cols);
-  result.zeros();
-
-  for (uint32_t i = 0 ; i < m.n_rows ; ++i) {
-    double sum = 0;
-    for (uint32_t j = 0 ; j < m.n_cols ; ++j) {
-      sum += m(i, j);
-    }
-    for (uint32_t j = 0 ; j < m.n_cols ; ++j) {
-      result(i, j) = sum;
-    }
-  }
-  return result;
-}
-
-mat cost(mat y, mat h) {
-  const mat exponential = nn::funcop(h, exp);
-  const mat sum = colsum(exponential);
-  const mat p = exponential % (1 / sum);
-  const mat J = - (y % nn::funcop(p, log));
-  //mat J = -(y % nn::funcop(h, log) + (1-y) % nn::funcop(1-h, log));
-  //const mat diff = y - h;
-  //const mat J = (diff % diff) / 2.0;
-  //mat J = -(y % h.transform(log) + (1-y) % nn::funcop(1-h, log)) / y.n_rows;
-  //mat J = -(y % h);
-  return J;
-}
-
-mat costd(mat y, mat a, mat) {
-  const mat exponential = nn::funcop(a, exp);
-  const mat sum = colsum(exponential);
-  const mat p = exponential % (1.0 / sum);
-  const mat delta = p - y;
-  //mat delta = (a - y);
-  //const mat delta = (a - y);
-  //mat delta = (a - y) / y.n_rows;
-  //mat grad = -(y % nn::funcop(z, sigmoidgrad));
-  return delta;
-}
-
 void load(mat &x, mat &y) {
   std::ifstream input("/home/joseph/C/data/movement_libras.dat", std::ios::in);
   std::string line;
@@ -159,20 +91,17 @@ double accuracy(mat answer, mat prediction) {
 }
 
 int main() {
-  double lrate = 1e-3;
-  const double lratedecay = 0.99;
-  const double lambda = 1e-1;
-  const int batchsize = 100;
+  double lrate = 1e-2;
+  //const double lratedecay = 0.99;
+  const double lambda = 1e-7;
 
   srand(time(NULL));
 
   InputLayer input(n);
   vector<Layer> hidden = {
-    //Layer(n, 32, lrate, lambda, atan, [] (double x) {return 1.0/(1.0+x*x);}),
-    //Layer(32, 8, lrate, lambda, atan, [] (double x) {return 1.0/(1.0+x*x);}),
-    Layer(n, 256, lrate, lambda, sigmoid, sigmoidgrad),
+    Layer(n, 256, lrate, lambda, nn::tanhyperbolic),
   };
-  OutputLayer output(256, o, lrate, lambda, identity, identitygrad, cost, costd);
+  nn::SoftmaxOutput output(256, o, lrate, lambda);
   NeuralNet nnet(input, output, hidden);
 
   mat x, y;
@@ -180,12 +109,13 @@ int main() {
   //cout << x << endl;
   //cout << y << endl;
 
+  //const int batchsize = 100;
   //nnet.feeddata(x.row(1), y.row(1), true);
   for (int i = 0 ; i < 60000 ; ++i) {
-    const int start = i % (datasize-batchsize);
-    const int end = start + batchsize;
-    nnet.feeddata(x.rows(start, end), y.rows(start, end), false);
-    //nnet.feeddata(x.row(i%datasize), y.row(i%datasize), false);
+    //const int start = i % (datasize-batchsize);
+    //const int end = start + batchsize;
+    //nnet.feeddata(x.rows(start, end), y.rows(start, end), false);
+    nnet.feeddata(x.row(i%datasize), y.row(i%datasize), false);
     //nnet.feeddata(x, y, false);
     cout << "\riteration: " << i+1 << " cost: " << nnet.computecost();
     if (i % datasize == 0) {
@@ -193,10 +123,10 @@ int main() {
       //cout << endl << nnet.getresult() << endl;
       //cout << y.row(i% datasize) << endl;
     }
-    if (i % 500 == 0) {
-      lrate *= lratedecay;
-      nnet.setlrate(lrate);
-    }
+    //if (i % 500 == 0) {
+      //lrate *= lratedecay;
+      //nnet.setlrate(lrate);
+    //}
   }
   cout << endl;
   mat result = nnet.predict(x);
