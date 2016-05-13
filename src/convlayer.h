410fb4d672ce60952f1fba5e796339fba05af102
kiddos <josephyu831007@yahoo.com.tw>
2016-05-13 23:41:54 +0800

    model conv layer

--- src/convlayer.h
+++ src/convlayer.h
@@ -8,10 +8,12 @@ namespace nn {
 class ConvLayer : public Layer {
  public:
   ConvLayer();
-  ConvLayer(const int nfilter, const int spatial,
+  ConvLayer(const int inputwidth, const int inputheight,
+            const int nfilter, const int spatial,
             const int stride, const int padding,
             const double lrate, func act, func actd);
-  ConvLayer(const int nfilter, const int spatial,
+  ConvLayer(const int inputwidth, const int inputheight,
+            const int nfilter, const int spatial,
             const int stride, const int padding,
             const double lrate, ActFunc actfunc);
   ConvLayer(const ConvLayer& conv);
@@ -25,6 +27,11 @@ class ConvLayer : public Layer {
   int getzeropadding() const;
 
  protected:
+  mat toimage(const mat& sample) const;
+  mat addzeropadding(const mat& image) const;
+  mat convolve(const mat& image, const int x, const int y) const;
+
+  int inputwidth, inputheight;
   int nfilter, spatial, stride, padding;
 };
 
