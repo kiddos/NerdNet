9ce92da1ea0a56913a6482e4a3f61ff20c841e22
kiddos <josephyu831007@yahoo.com.tw>
2016-05-14 17:02:58 +0800

    separate trainer object

---
+++ src/trainer.cpp
@@ -0,0 +1,46 @@
+#include "trainer.h"
+
+namespace nn {
+
+Trainer::Trainer() : nnet(nullptr), iters(0), step(0), usedecay(false), k(0), r0(0) {}
+
+Trainer::Trainer(NeuralNet& nnet) : nnet(&nnet), iters(0), step(0),
+                                    usedecay(false), k(0), r0(0) {}
+
+Trainer::Trainer(NeuralNet& nnet, double r0, double k, unsigned long step)
+    : nnet(&nnet), iters(0), step(step), usedecay(true), k(k), r0(r0) {}
+
+Trainer::Trainer(const Trainer& trainer)
+    : nnet(trainer.nnet), iters(trainer.iters), step(trainer.step),
+      usedecay(trainer.usedecay), k(trainer.k), r0(trainer.r0) {}
+
+Trainer& Trainer::operator= (const Trainer& trainer) {
+  nnet = trainer.nnet;
+  iters = trainer.iters;
+  step = trainer.step;
+  usedecay = trainer.usedecay;
+  k = trainer.k;
+  r0 = trainer.r0;
+  return *this;
+}
+
+Trainer::~Trainer() {
+  nnet = nullptr;
+}
+
+bool Trainer::gradcheck(const mat& x, const mat& y) {
+  return nnet->gradcheck(x, y);
+}
+
+void Trainer::feeddata(const mat& x, const mat& y) {
+  iters ++;
+  if (usedecay && iters == step) {
+    nnet->setlrate(r0 * exp(-k*iters));
+  }
+
+  nnet->forwardprop(x);
+  nnet->backprop(y);
+  nnet->update();
+}
+
+}
---
+++ src/trainer.h
@@ -0,0 +1,31 @@
+#ifndef TRAINER_H
+#define TRAINER_H
+
+#include <vector>
+#include <math.h>
+#include "neuralnet.h"
+
+namespace nn {
+
+class Trainer {
+ public:
+  Trainer();
+  explicit Trainer(NeuralNet& nnet);
+  Trainer(NeuralNet& nnet, double r0, double k, unsigned long step);
+  Trainer(const Trainer& trainer);
+  Trainer& operator= (const Trainer& trainer);
+  ~Trainer();
+
+  virtual bool gradcheck(const mat& x, const mat& y);
+  virtual void feeddata(const mat& x, const mat& y);
+
+ protected:
+  NeuralNet* nnet;
+  unsigned long iters, step;
+  bool usedecay;
+  double k, r0;
+};
+
+}
+
+#endif /* end of include guard: TRAINER_H */
