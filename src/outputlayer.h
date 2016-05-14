3d0a2b4c81bf873aa7b35908cf505eaa6352bddf
kiddos <josephyu831007@yahoo.com.tw>
2016-05-15 02:23:52 +0800

    move to other file

--- src/outputlayer.h
+++ src/outputlayer.h
@@ -31,51 +31,6 @@ class OutputLayer : public Layer {
   mat y;
 };
 
-/*** Softmax Output ***/
-class SoftmaxOutput : public OutputLayer {
- public:
-  SoftmaxOutput();
-  SoftmaxOutput(const SoftmaxOutput& output);
-  SoftmaxOutput(const int pnnodes, const int outputnodes,
-                const double lrate, const double lambda);
-
-  static mat costfunc(mat y, mat h);
-  static mat costfuncdelta(mat y, mat a, mat z);
-};
-
-class QuadraticOutput : public OutputLayer {
- public:
-  QuadraticOutput();
-  QuadraticOutput(const QuadraticOutput& output);
-  QuadraticOutput(const int pnnodes, const int outputnodes,
-                  const double lrate, const double lambda);
-
-  static mat costfunc(mat y, mat h);
-  static mat costfuncdelta(mat y, mat a, mat z);
-};
-
-class CrossEntropyOutput : public OutputLayer {
- public:
-  CrossEntropyOutput();
-  CrossEntropyOutput(const CrossEntropyOutput& output);
-  CrossEntropyOutput(const int pnnodes, const int outputnodes,
-                     const double lrate, const double lambda);
-
-  static mat costfunc(mat y, mat h);
-  static mat costfuncdelta(mat y, mat a, mat z);
-};
-
-class KullbackLeiblerOutput : public OutputLayer {
- public:
-  KullbackLeiblerOutput();
-  KullbackLeiblerOutput(const KullbackLeiblerOutput& output);
-  KullbackLeiblerOutput(const int pnnodes, const int outputnodes,
-                        const double lrate, const double lambda);
-
-  static mat costfunc(mat y, mat h);
-  static mat costfuncdelta(mat y, mat a, mat z);
-};
-
 } /* end of nn namespace */
 
 #endif /* end of include guard: OUTPUTLAYER_H */
