8b2f6838ae73b557d46ad515e5165a00847dd08c
kiddos <josephyu831007@yahoo.com.tw>
2016-05-15 02:24:07 +0800

    more layer and trainer

--- src/nnet.h
+++ src/nnet.h
@@ -5,8 +5,19 @@
 #include "convlayer.h"
 #include "softmaxlayer.h"
 #include "inputlayer.h"
+
 #include "outputlayer.h"
+#include "softmaxoutput.h"
+#include "crossentropyoutput.h"
+#include "quadraticoutput.h"
+#include "kullbackleibleroutput.h"
+
 #include "neuralnet.h"
+
+#include "trainer.h"
+#include "momentumtrainer.h"
+#include "sgdtrainer.h"
+#include "batchtrainer.h"
 
 #endif /* end of include guard: NNET_H */
 
