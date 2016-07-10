c2fac5c47131a7574ed540fd6794cda270e7b917
kiddos <josephyu831007@yahoo.com.tw>
2016-07-10 17:23:37 +0800

    use reference input for mat op

--- src/matop.cpp
+++ src/matop.cpp
@@ -2,19 +2,19 @@
 
 namespace nn {
 
-mat funcop(const mat m, func f) {
+mat funcop(const mat& m, func f) {
   mat newmat = m;
   newmat.transform(f);
   return newmat;
 }
 
-mat addcol(const mat m) {
+mat addcol(const mat& m) {
   mat newmat(m.n_rows, m.n_cols+1, arma::fill::ones);
   newmat.submat(0, 1, newmat.n_rows-1, newmat.n_cols-1) = m;
   return newmat;
 }
 
-mat addcols(const mat m, const int index, const double value) {
+mat addcols(const mat& m, const int index, const double value) {
   mat newmat = m;
   mat values;
   if (value == 0) {
@@ -32,14 +32,14 @@ mat addcols(const mat m, const int index, const double value) {
   return newmat;
 }
 
-mat addrow(const mat m) {
+mat addrow(const mat& m) {
   mat newmat = m;
   mat values(1, m.n_cols, arma::fill::ones);
   newmat.insert_rows(0, values);
   return newmat;
 }
 
-mat addrows(const mat m, const int index, const double value) {
+mat addrows(const mat& m, const int index, const double value) {
   mat newmat = m;
   mat values;
   if (value == 0) {
@@ -57,7 +57,7 @@ mat addrows(const mat m, const int index, const double value) {
   return newmat;
 }
 
-double sumall(const mat m) {
+double sumall(const mat& m) {
   const mat temp = m;
   return arma::accu(temp);
 }
@@ -67,7 +67,7 @@ mat normalize(const mat& m) {
 }
 
 mat norm(const mat& m) {
-  return arma::sqrt(arma::sum(m % m, 1));
+  return arma::repmat(arma::sqrt(arma::sum(m % m, 1)), 1, m.n_cols);
 }
 
 }
--- src/matop.h
+++ src/matop.h
@@ -2,12 +2,12 @@
 
 namespace nn {
 
-mat funcop(const mat m, func f);
-mat addcol(const mat m);
-mat addcols(const mat m, const int index, const double value);
-mat addrow(const mat m);
-mat addrows(const mat m, const int index, const double value);
-double sumall(const mat m);
+mat funcop(const mat& m, func f);
+mat addcol(const mat& m);
+mat addcols(const mat& m, const int index, const double value);
+mat addrow(const mat& m);
+mat addrows(const mat& m, const int index, const double value);
+double sumall(const mat& m);
 mat normalize(const mat& m);
 mat norm(const mat& m);
 
