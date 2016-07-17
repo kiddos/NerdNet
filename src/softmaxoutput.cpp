604fa6aa8bec6d192660ff0cb48ded64a66530c6
kiddos <josephyu831007@yahoo.com.tw>
2016-07-17 11:39:03 +0800

    use wrapper function

--- src/softmaxoutput.cpp
+++ src/softmaxoutput.cpp
@@ -14,16 +14,16 @@ SoftmaxOutput::SoftmaxOutput(const int pnnodes, const int outputnodes,
                   SoftmaxOutput::costfuncdelta) {}
 
 mat SoftmaxOutput::costfunc(mat y, mat h) {
-  const mat expo = arma::exp(h);
-  const mat sumexpo = arma::repmat(arma::sum(expo, 1), 1, y.n_cols);
+  const mat expo = exponential(h);
+  const mat sumexpo = repeat(rowsum(expo), 1, y.n_cols);
   const mat P = expo % (1.0 / sumexpo);
   const mat J = - (y % arma::log(P)) / y.n_rows;
   return J;
 }
 
 mat SoftmaxOutput::costfuncdelta(mat y, mat a, mat) {
-  const mat expo = arma::exp(a);
-  const mat sumexpo = arma::repmat(arma::sum(expo, 1), 1, y.n_cols);
+  const mat expo = exponential(a);
+  const mat sumexpo = repeat(rowsum(expo), 1, y.n_cols);
   const mat P = expo % (1.0 / sumexpo);
   const mat delta = P - y;
   return delta / y.n_rows;
