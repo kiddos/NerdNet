8a80364712b7386bfd22b4608c081701203813d6
kiddos <kiddo831007@gmail.com>
2017-03-11 11:33:02 +0800

    basic ops header

---
+++ src/tensor/ops/basic_ops.h
@@ -0,0 +1,9 @@
+#ifndef TENSOR_BASIC_OPS_H
+#define TENSOR_BASIC_OPS_H
+
+#include "tensor/ops/add.h"
+#include "tensor/ops/sub.h"
+#include "tensor/ops/mul.h"
+#include "tensor/ops/div.h"
+
+#endif /* end of include guard: TENSOR_BASIC_OPS_H */
