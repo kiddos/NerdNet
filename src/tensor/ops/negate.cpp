ddfbbcbb07cdae7db9a8daf3b41843c8b80dd799
kiddos <kiddo831007@gmail.com>
2017-03-11 13:45:05 +0800

    negation

---
+++ src/tensor/ops/negate.cpp
@@ -0,0 +1,27 @@
+#include "tensor/ops/negate.h"
+
+namespace nn {
+namespace tensor {
+
+template <typename DType>
+Tensor<DType> operator-(const Tensor<DType>& t) {
+  Tensor<DType> output(t.shape());
+  int chunk = t.shape().chunk(0);
+  for (int i = 0 ; i < chunk ; ++i) {
+    output[i] = - t.data(i);
+  }
+  return output;
+}
+
+template <typename DType>
+Tensor<DType> neg(const Tensor<DType>& t) {
+  return -t;
+}
+
+template Tensor<float> operator-(const Tensor<float>& t);
+template Tensor<double> operator-(const Tensor<double>& t);
+template Tensor<float> neg(const Tensor<float>& t);
+template Tensor<double> neg(const Tensor<double>& t);
+
+} /* end of tensor namespace */
+} /* end of nn namespace */
