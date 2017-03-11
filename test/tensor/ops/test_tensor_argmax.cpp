dfe256947c8371db062b29b94294ae9b511601e2
kiddos <kiddo831007@gmail.com>
2017-03-11 13:44:14 +0800

    update argmax test

--- test/tensor/ops/test_tensor_argmax.cpp
+++ test/tensor/ops/test_tensor_argmax.cpp
@@ -2,7 +2,6 @@
 #include "tensor/ops/argmax.h"
 #include "tensor/ops/mul.h"
 
-
 template <typename DType>
 void TestArgmax(int size) {
   using namespace nn::tensor;
@@ -10,28 +9,26 @@ void TestArgmax(int size) {
   Tensor<DType> t = Tensor<DType>::Eyes(size) * static_cast<DType>(10);
 
   Tensor<DType> argmax = nn::tensor::argmax(t, 1);
-  for (int i = 0 ; i < size ; ++i) {
+  for (int i = 0; i < size; ++i) {
     EXPECT_EQ(argmax[i], 10);
   }
 
   argmax = nn::tensor::argmax(t, 0);
-  for (int i = 0 ; i < size ; ++i) {
+  for (int i = 0; i < size; ++i) {
     EXPECT_EQ(argmax[i], 10);
   }
 }
 
-TEST(TestTensorOps, ArgmaxSmallScale) {
-  TestArgmax<float>(10);
-  TestArgmax<double>(10);
-
-  TestArgmax<float>(100);
-  TestArgmax<double>(100);
+TEST(TestTensorArgmax, SmallScale) {
+  for (int i = 1; i < 64; ++i) {
+    TestArgmax<float>(i);
+    TestArgmax<double>(i);
+  }
 }
 
-TEST(TestTensorOps, ArgmaxLargeScale) {
-  TestArgmax<float>(1000);
-  TestArgmax<double>(1000);
-
-  TestArgmax<float>(10000);
-  TestArgmax<double>(10000);
+TEST(TestTensorArgmax, LargeScale) {
+  for (int i = 64; i <= 1024; ++i) {
+    TestArgmax<float>(i);
+    TestArgmax<double>(i);
+  }
 }
