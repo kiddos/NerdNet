fadf5bde25f2678ecdb869fbd320d7b220b0ad9b
kiddos <kiddo831007@gmail.com>
2017-03-11 11:33:20 +0800

    all operations

---
+++ src/tensor/ops/ops.h
@@ -0,0 +1,16 @@
+#ifndef TENSOR_OPS_H
+#define TENSOR_OPS_H
+
+#include "tensor/ops/argmax.h"
+#include "tensor/ops/basic_ops.h"
+#include "tensor/ops/equal.h"
+#include "tensor/ops/exp.h"
+#include "tensor/ops/log.h"
+#include "tensor/ops/matmul.h"
+#include "tensor/ops/negate.h"
+#include "tensor/ops/power.h"
+#include "tensor/ops/reduce_mean.h"
+#include "tensor/ops/reduce_sum.h"
+#include "tensor/ops/sqrt.h"
+
+#endif /* end of include guard: TENSOR_OPS_H */
