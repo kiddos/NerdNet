50323946b44678197634f86171754e96644feef1
kiddos <kiddo831007@gmail.com>
2017-08-18 09:57:23 +0800

    layer collection

---
+++ src/NerdNet/layer/layers.h
@@ -0,0 +1,17 @@
+#ifndef LAYERS_H
+#define LAYERS_H
+
+#include "NerdNet/layer/arctan_layer.h"
+#include "NerdNet/layer/fc_layer.h"
+#include "NerdNet/layer/input_layer.h"
+#include "NerdNet/layer/leaky_relu_layer.h"
+#include "NerdNet/layer/mean_square_error.h"
+#include "NerdNet/layer/relu_layer.h"
+#include "NerdNet/layer/sigmoid_layer.h"
+#include "NerdNet/layer/smooth_relu_layer.h"
+#include "NerdNet/layer/tanh_layer.h"
+
+#include "NerdNet/layer/constant_initializer.h"
+#include "NerdNet/layer/normal_initializer.h"
+
+#endif /* end of include guard: LAYERS_H */
