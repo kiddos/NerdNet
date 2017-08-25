94d74a589a3ed653d8e35db735a9631074c830ad
kiddos <kiddo831007@gmail.com>
2017-08-25 09:14:38 +0800

    install new armadillo for test build

---
+++ .travis/install_armadillo_7x.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+wget http://sourceforge.net/projects/arma/files/armadillo-7.960.1.tar.xz
+tar xvf armadillo-7.960.1.tar.xz
+cd armadillo-7.960.1
+mkdir build
+cmake ..
+make -j8
+sudo make install
+cd ..
