32eed7abfc1ab1a9f57a6e964347a0417fc9edc0
kiddos <kiddo831007@gmail.com>
2017-08-25 10:11:02 +0800

    test sgd trainer

---
+++ test/unit/trainer/test_sgd_trainer.cc
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "NerdNet/convert.h"
+#include "NerdNet/layers.h"
+#include "NerdNet/nerd_net.h"
+#include "NerdNet/trainer/sgd_trainer.h"
+
+using nerd::nn::NerdNet;
+using nerd::nn::FCLayer;
+using nerd::nn::ReluLayer;
+using nerd::nn::MeanSquareError;
+using nerd::nn::VariableShape;
+using nerd::nn::Tensor;
+using nerd::nn::trainer::SGDTrainer;
+using arma::Mat;
+
+class SGDTrainerTest : public ::testing::Test {
+ public:
+  enum {
+    DATA_SIZE = 64,
+    INPUT_SIZE = 4,
+    HIDDEN_SIZE = 16,
+    OUTPUT_SIZE = 8,
+    ITERATIONS = 10000
+  };
+
+  SGDTrainerTest() : trainer_(1e-4) {
+    trainer_.set_verbose(true);
+  }
+
+ protected:
+  void SetUp() override {
+    nerdnet_ = std::make_shared<NerdNet>();
+    nerdnet_->AddLayer<FCLayer>(VariableShape{INPUT_SIZE, HIDDEN_SIZE});
+    nerdnet_->AddLayer<ReluLayer>();
+    nerdnet_->AddLayer<FCLayer>(VariableShape{HIDDEN_SIZE, HIDDEN_SIZE});
+    nerdnet_->AddLayer<ReluLayer>();
+    nerdnet_->AddLayer<MeanSquareError>(
+        VariableShape{HIDDEN_SIZE, OUTPUT_SIZE});
+
+    trainer_.set_nerdnet(nerdnet_);
+
+    Mat<float> test_data(DATA_SIZE, INPUT_SIZE),
+        test_label(DATA_SIZE, OUTPUT_SIZE);
+    test_data.randn();
+    test_label.randn();
+    Matrix2Tensor(test_data, test_data_tensor_);
+    Matrix2Tensor(test_label, test_label_tensor_);
+  }
+
+  Tensor<float> test_data_tensor_, test_label_tensor_;
+  std::shared_ptr<NerdNet> nerdnet_;
+  SGDTrainer trainer_;
+};
+
+TEST_F(SGDTrainerTest, Convergence) {
+  float start_cost = trainer_.Train(test_data_tensor_, test_label_tensor_);
+
+  for (int i = 0; i < ITERATIONS; ++i) {
+    trainer_.Train(test_data_tensor_, test_label_tensor_);
+  }
+  float end_cost = trainer_.Train(test_data_tensor_, test_label_tensor_);
+  EXPECT_LT(end_cost, start_cost);
+}
