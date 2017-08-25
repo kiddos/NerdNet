c95271d1b5796bdf503d9775a7d8f75ceb5eb612
kiddos <kiddo831007@gmail.com>
2017-08-25 09:34:38 +0800

    fix install script

--- .travis/install_armadillo_7x.sh
+++ .travis/install_armadillo_7x.sh
@@ -4,6 +4,7 @@ wget http://sourceforge.net/projects/arma/files/armadillo-7.960.1.tar.xz
 tar xvf armadillo-7.960.1.tar.xz
 cd armadillo-7.960.1
 mkdir build
+cd build
 cmake ..
 make -j8
 sudo make install
